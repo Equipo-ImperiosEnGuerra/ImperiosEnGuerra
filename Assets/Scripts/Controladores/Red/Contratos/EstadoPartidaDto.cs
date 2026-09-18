8a1f982d9dc0fad7eea05be2ec2c60e7f4bed78a
Juli <julianita.gutierrez2005@gmail.com>
2026-09-18 08:52:48 -0500

    feat: exponer estado y orden de unidad por API

--- Assets/Scripts/Controladores/Red/Contratos/EstadoPartidaDto.cs
+++ Assets/Scripts/Controladores/Red/Contratos/EstadoPartidaDto.cs
@@ -58,7 +58,10 @@ namespace ImperiosEnGuerra.Controladores.Red.Contratos
         public string tipo;
         public CoordenadaEstadoDto coordenada;
         public bool disponible;
-    }
+
+        public string estado;
+        public string ordenActiva;
+}
 
     [Serializable]
     public class CoordenadaEstadoDto
