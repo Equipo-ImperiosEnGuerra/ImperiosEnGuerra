7e0885ef3e48d1a2df44f345a3a9f4cd8a3d5479
cy2tmigue <miguelpuentes2808@gmail.com>
2026-09-21 01:21:09 -0500

    fix: completar imports de pruebas integradas etapa 4.5

--- tests/ImperiosEnGuerra.Tests/Etapa45IntegracionTests.cs
+++ tests/ImperiosEnGuerra.Tests/Etapa45IntegracionTests.cs
@@ -1,5 +1,6 @@
 using ImperiosEnGuerra.Api.Contratos;
 using ImperiosEnGuerra.Api.Servicios;
+using ImperiosEnGuerra.Modelo.Acciones;
 using ImperiosEnGuerra.Modelo.Core;
 using ImperiosEnGuerra.Modelo.Edificios;
 using ImperiosEnGuerra.Modelo.Map;
