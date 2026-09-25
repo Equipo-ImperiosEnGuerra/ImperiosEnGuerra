f7c408c523600de1f456073b3bb734569a71f856
cy2tmigue <miguelpuentes2808@gmail.com>
2026-09-25 14:21:43 -0500

    feat: agregar contratos y operacion de curacion

---
+++ Assets/Scripts/Controladores/Red/Contratos/CuracionDto.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ImperiosEnGuerra.Controladores.Red.Contratos
+{
+    [Serializable]
+    public class CuracionDto
+    {
+        public string curadorId;
+        public string objetivoId;
+    }
+}
