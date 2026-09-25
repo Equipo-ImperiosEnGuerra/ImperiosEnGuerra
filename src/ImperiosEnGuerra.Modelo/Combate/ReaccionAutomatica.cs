34f6654fc39565feffab42858082ed550e9c1a25
cy2tmigue <miguelpuentes2808@gmail.com>
2026-09-25 15:48:01 -0500

    feat: representar movimiento idle automatico

--- src/ImperiosEnGuerra.Modelo/Combate/ReaccionAutomatica.cs
+++ src/ImperiosEnGuerra.Modelo/Combate/ReaccionAutomatica.cs
@@ -1,11 +1,13 @@
 using System;
+using ImperiosEnGuerra.Modelo.Map;
 
 namespace ImperiosEnGuerra.Modelo.Combate
 {
     public enum TipoReaccionAutomatica
     {
         Atacar,
-        Curar
+        Curar,
+        MoverIdle
     }
 
     public sealed class ReaccionAutomatica
@@ -13,6 +15,7 @@ namespace ImperiosEnGuerra.Modelo.Combate
         public TipoReaccionAutomatica Tipo { get; }
         public Guid UnidadId { get; }
         public Guid ObjetivoId { get; }
+        public Coordenada Destino { get; }
 
         public ReaccionAutomatica(
             TipoReaccionAutomatica tipo,
@@ -32,6 +35,28 @@ namespace ImperiosEnGuerra.Modelo.Combate
             Tipo = tipo;
             UnidadId = unidadId;
             ObjetivoId = objetivoId;
+            Destino = null;
+        }
+
+        public ReaccionAutomatica(
+            Guid unidadId,
+            Coordenada destino)
+        {
+            if (unidadId == Guid.Empty)
+                throw new ArgumentException(
+                    "La unidad debe tener un ID válido.",
+                    nameof(unidadId));
+
+            if (destino == null)
+                throw new ArgumentNullException(
+                    nameof(destino));
+
+            Tipo =
+                TipoReaccionAutomatica.MoverIdle;
+
+            UnidadId = unidadId;
+            ObjetivoId = Guid.Empty;
+            Destino = destino;
         }
     }
 }
